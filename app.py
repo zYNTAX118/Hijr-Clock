e3e8ac99a07360386f9ddd4711889eae630ba688
Izzet <izzetrizal751@gmail.com>
2025-04-25 16:43:51 +0700

    add secret page for dev testing

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
